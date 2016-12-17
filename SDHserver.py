98ba0a86b37065de92e1ce402e9aa94221f252c1
Marcin <sendtopolish@gmail.com>
2016-12-17 02:32:33 +0000

    Back ground color, concept of icons and their interpretation.

--- SDHserver.py
+++ SDHserver.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
