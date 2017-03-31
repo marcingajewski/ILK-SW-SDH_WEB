b1379e0110aea417db2f8a306b58e57cf92066b7
Marcin Gajewski <marcin.gajewski@romaxtech.com>
2017-04-01 00:41:06 +0100

    tmpRAM folder for 1s write/read operations. 2 gauges to display inner temp and hum.

--- SDHserver.py
+++ SDHserver.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/_lastRecordSQlite')
 def retrieveData():
     # command = request.args.get('command', 0, type=int)
-    conn = sqlite3.connect('/usr/local/SmartDogHouse/sdhdatabase.db')
+    conn = sqlite3.connect('/usr/local/SmartDogHouse/tmpRAM/sdhdatabase.db')
     cursor = conn.execute("SELECT " +
                           "light1_lx, pres1_hPA, pres2_hPa, temp1_C, temp2_C, temp3_C, hum1_per, hum2_per, " +
                           "food, water, pir1, pir2, vol24V, cur24V_mA, pwr24V_W, pwrLED1, pwrFan1,pwrHeater1" +
--- static/js/main.js
+++ static/js/main.js
@@ -16,7 +16,9 @@ function retrieve() {
         gg1_light.refresh(data.light1_lx);
         gg2_pressure.refresh(data.pres1_hPA);
         gg3_temperature.refresh(data.temp1_C);
+        gg9_temperature.refresh(data.temp2_C);
         gg4_humidity.refresh(data.hum1_per);
+        gg10_humidity.refresh(data.hum2_per);
         gg7_power.refresh(data.vol24V * data.cur24V_mA/1000);
         gg8_led1.refresh(data.pwrLED1);
         gg5_voltage.refresh(data.vol24V);
